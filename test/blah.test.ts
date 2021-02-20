7b8430926fc68c57817ecc3f288f9c4edb50d713
Elias Rangel <propstle@gmail.com>
2021-02-19 20:16:15 -0600

    f

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -2,5 +2,5 @@ import { Base64Decode } from "../src/utilities";
 
 // function Base64Encode(data: Uint8Array): string {
 test('encodes base64', () => {
-  expect(Base64Decode("hello")).expect("aGVsbG8=")
+  expect(Base64Decode("hello")).toBe("aGVsbG8=")
 })
